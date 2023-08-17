a1971bcbe68f6d1df17b7b82b0860f43d0c8d754
“Leonardo <leonardo.gurdulich@halopowered.com>
2023-08-17 10:54:23 -0300

    Trabajando con form dinamico

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 250,
+  inStorage: 10
+}
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
 // public myForm: UntypedFormGroup = new UntypedFormGroup({
 //   name: new FormControl('',[],[]),
@@ -15,15 +20,47 @@ export class BasicPageComponent {
 // })
 
 public myForm: FormGroup = this.fb.group({
-  name: [''],
-  price: [0],
-  inStorage: [0],
+  name: ['', [Validators.required, Validators.minLength(3)]],
+  price: [0, [Validators.required, Validators.min(0)]],
+  inStorage: [0, [Validators.required, Validators.min(0)]],
 })
 
 constructor( private fb: FormBuilder){}
 
+ngOnInit(): void {
+  this.myForm.reset(rtx5090);
+}
+
+isValidField( field: string): boolean|null {
+  return this.myForm.controls[field].errors 
+    && this.myForm.controls[field].touched;
+}
+
+getFieldError( field: string): string | null {
+if(!this.myForm.controls[field]) return null;
+  
+const errors = this.myForm.controls[field].errors || {};
+
+for (const key of Object.keys(errors)){
+  switch(key){
+    case 'required':
+      return 'El campo es requerido';
+    case 'minlength':
+      return `El campo debe ser mayor a ${ errors['minlength'].requiredLength } caracteres`;
+  }
+}
+return 'e';
+}
+
 onSave(): void {
+  if(this.myForm.invalid) {
+    this.myForm.markAllAsTouched();
+    return;
+  };
+
   console.log(this.myForm.value);
+
+  this.myForm.reset({ price: 0, inStorage: 0});
 }
 
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicPageComponent implements OnInit {
 
-  constructor() { }
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Horizon Zero Down', Validators.required],
+      ['Horizon Forbidden West', Validators.required]
+    ])
+  })
+
+  constructor( private fb: FormBuilder) { }
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField( field: string): boolean|null {
+    return this.myForm.controls[field].errors 
+      && this.myForm.controls[field].touched;
+  }
+  
+  isValidFieldInArray(formArray : FormArray, index: number){
+    return formArray.controls[index].errors 
+    && formArray.controls[index].touched;
+  }
+
+  getFieldError( field: string): string | null {
+  if(!this.myForm.controls[field]) return null;
+    
+  const errors = this.myForm.controls[field].errors || {};
+  
+  for (const key of Object.keys(errors)){
+    switch(key){
+      case 'required':
+        return 'El campo es requerido';
+      case 'minlength':
+        return `El campo debe ser mayor a ${ errors['minlength'].requiredLength } caracteres`;
+    }
+  }
+  return 'e';
+  }
+
 
   ngOnInit(): void {
+  }
+
+  onDeleteFavorite(index : number){
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit(): void {
+
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
   }
 
 }
